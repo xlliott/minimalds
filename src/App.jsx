2c5742b13b35815a9cd453ce504179cec821520e
xlliott <campbellholdco@gmail.com>
2026-06-01 21:54:29 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react';
+import { ThemeProvider } from './context/ThemeContext';
+import Layout from './components/Layout/Layout';
+import Home from './pages/Home';
+import About from './pages/About';
+import ReleaseNotes from './pages/ReleaseNotes';
+import '../../src/theme/tokens.css';
+import './App.css';
+
+function Router() {
+  const [path, setPath] = useState(window.location.hash || '#/');
+
+  useEffect(() => {
+    const handleHashChange = () => setPath(window.location.hash || '#/');
+    window.addEventListener('hashchange', handleHashChange);
+    return () => window.removeEventListener('hashchange', handleHashChange);
+  }, []);
+
+  const renderPage = () => {
+    switch (path) {
+      case '#/about':        return <About />;
+      case '#/release-notes': return <ReleaseNotes />;
+      default:               return <Home />;
+    }
+  };
+
+  return (
+    <Layout currentPath={path}>
+      {renderPage()}
+    </Layout>
+  );
+}
+
+export default function App() {
+  return (
+    <ThemeProvider>
+      <Router />
+    </ThemeProvider>
+  );
+}
