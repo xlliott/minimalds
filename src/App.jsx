16d093dbe00e75f0beaaff2c000d7a92ce33efca
xlliott <campbellholdco@gmail.com>
2026-06-09 11:17:17 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import About from './pages/About';
 import ReleaseNotes from './pages/ReleaseNotes';
 import Colour from './pages/Colour';
 import Typography from './pages/Typography';
+import Icons from './pages/Icons';
 import AccordionPage from './pages/AccordionPage';
 import './theme/tokens.css';
 import './App.css';
@@ -25,6 +26,7 @@ function Router() {
       case '#/release-notes': return <ReleaseNotes />;
       case '#/colour':        return <Colour />;
       case '#/typography':    return <Typography />;
+      case '#/icons':         return <Icons />;
       case '#/accordion':     return <AccordionPage />;
       default:                return <Home />;
     }
