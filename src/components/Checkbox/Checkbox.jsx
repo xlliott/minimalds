870f730c56be86c1bdc208dc24876804fca0be7f
xlliott <campbellholdco@gmail.com>
2026-06-09 21:15:18 +0100

    Update Checkbox.jsx

--- src/components/Checkbox/Checkbox.jsx
+++ src/components/Checkbox/Checkbox.jsx
@@ -17,7 +17,7 @@ const checkIcon = (
   <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
       d="M2 6L5 9L10 3"
-      stroke="white"
+      stroke="currentColor"
       strokeWidth="1.5"
       strokeLinecap="round"
       strokeLinejoin="round"
